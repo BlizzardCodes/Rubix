5dbf48e53d0aa081ba426c12e71115d1c5971889
BlizzardCodes <caleb@gmail.com>
2022-05-10 15:38:30 -0700

    extracted sideColors from cube3 to sidecolors.js

--- public/cube3.js
+++ public/cube3.js
@@ -1,3 +1,20 @@
+/*
+Easy Parts
+- Keyboard input. (If i press f, then the cube would rotate the front side clockwise)
+- Rotation around the center of the cube, not the corner.
+- Make floating arrow to define with side is up.
+- Tracking which colored cube is in which physical location.
+
+
+Hard Parts
+- Rotate a face with animations.
+- Update the data model.
+
+Parts We May Not Get To
+- Autosolving cube.
+- Randomizing cube.
+*/
+
 const { enable3d, Scene3D, Canvas, THREE } = ENABLE3D;
 
 class MainScene extends Scene3D {
@@ -14,7 +31,6 @@ class MainScene extends Scene3D {
     this.blockOffset = new THREE.Vector3();
     this.prev == { x: 0, y: 0 };
   }
-
   async create() {
     // this.third.warpSpeed('-orbitControls')
 
@@ -27,243 +43,20 @@ class MainScene extends Scene3D {
     hemisphereLight.intensity = intensity;
     ambientLight.intensity = intensity;
     directionalLight.intensity = intensity;
-    
-    const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xffcccc });
-    const redMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    const orangeMaterial = new THREE.MeshBasicMaterial({ color: 0xfa5000 });
-    const yellowMaterial = new THREE.MeshBasicMaterial({ color: 0xfff000 });
-    const greenMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const blueMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    const blackMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
-    
+
+    this.third.camera.lookAt(2, 2, 2);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const sideColors = [
-      [
-        blackMaterial,
-        blueMaterial,
-        blackMaterial,
-        yellowMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        blackMaterial,
-        yellowMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        blackMaterial,
-        yellowMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        blackMaterial,
-        blackMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        whiteMaterial,
-        blackMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        whiteMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blueMaterial,
-        whiteMaterial,
-        blackMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        yellowMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        yellowMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        yellowMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        whiteMaterial,
-        blackMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        whiteMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        blackMaterial,
-        blackMaterial,
-        whiteMaterial,
-        blackMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        blackMaterial,
-        yellowMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        blackMaterial,
-        yellowMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        blackMaterial,
-        yellowMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        whiteMaterial,
-        blackMaterial,
-        blackMaterial,
-        orangeMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        whiteMaterial,
-        blackMaterial,
-        blackMaterial,
-        blackMaterial,
-      ],
-      [
-        greenMaterial,
-        blackMaterial,
-        whiteMaterial,
-        blackMaterial,
-        redMaterial,
-        blackMaterial,
-      ],
-    ];
-    const blocks = [];
+
+    let sideColors = getSideColors();
+
     const CUBE_SIZE = 3;
     let cubeCount = 0;
     for (let i = 0; i < CUBE_SIZE; i++) {
-      for (let j = 0.5; j < CUBE_SIZE; j++) {
+      for (let j = 0; j < CUBE_SIZE; j++) {
         for (let k = 0; k < CUBE_SIZE; k++) {
           const cube = new THREE.Mesh(geometry, sideColors[cubeCount]);
-          cube.position.set(i, j, k);
+          cube.position.set(i - 1.5, j - 1.5, k - 1.5);
           this.third.add.existing(cube); //magic line
           cubeCount++;
         }
@@ -271,7 +64,6 @@ class MainScene extends Scene3D {
     }
   }
 }
-
 const config = {
   type: Phaser.WEBGL,
   transparent: true,
